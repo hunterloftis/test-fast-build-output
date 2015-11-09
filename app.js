2aa18836d4cab7d4130493f709914159c22e2430
Hunter Loftis <hunter@hunterloftis.com>
2015-11-09 18:42:08 -0500

    add a big post

--- app.js
+++ app.js
@@ -19,14 +19,17 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', function(req, res, next) { res.send({ status: 'ok' }); });
 app.use('/', routes);
 app.use('/users', users);
+app.post('/bigpost', function(req, res, next) {
+  res.status(404).send('not here');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
