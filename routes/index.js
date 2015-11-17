76d01029ff42a2885793ce5757b7e742b8b32713
Hunter Loftis <hunter@hunterloftis.com>
2015-11-17 14:48:05 -0500

    should-rebuild

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bar-foo2' });
+  res.render('index', { title: 'Should-rebuild' });
 });
 
 module.exports = router;
